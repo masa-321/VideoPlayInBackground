bdc78a65d28bdd1dd710ffde2e87729d356a3087
masa-321 <ma.ly321.luna.notte@gmail.com>
2020-12-15 06:57:49 +0900

    [modify]背景動画を設定＋UIImageViewやUILableが前面に表示されるようにした。

--- VideoPlayInBackground/ViewController.swift
+++ VideoPlayInBackground/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var videoLayer: UIView!
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet weak var signIn: UIButton!
+    @IBOutlet weak var stack: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playVideo()
+        
+    }
+    
+    func playVideo() {
+        guard let path = Bundle.main.path(forResource: "intro", ofType: "mp4") else {
+            return
+        }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        self.videoLayer.layer.addSublayer(playerLayer)
+        
+        player.play()
+        
+        videoLayer.bringSubviewToFront(img) //なぜか上に表示されない…
+        videoLayer.bringSubviewToFront(lbl)
+        //videoLayer.bringSubviewToFront(signIn)
+        //videoLayer.bringSubviewToFront(signUp)
+        //videoLayer.bringSubviewToFront(stack)
     }
 
 
 }
-
